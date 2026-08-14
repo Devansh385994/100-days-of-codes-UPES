331151f074cf5a964a9e930569e488448c42ae70
Devansh385994 <devansh.38594@stu.upes.ac.in>
2026-08-14 16:04:32 +0530

    Update Q1.c

--- Day_1/Q1.c
+++ Day_1/Q1.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+//input two numbers and display their sum
+int main()
+{
+int a,b;
+printf("a :");
+scanf("%d",&a);
 
+printf("b :");
+scanf("%d",&b);
+
+printf("sum :%d", a+b);
+return 0;
+}

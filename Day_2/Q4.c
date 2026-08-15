e27909a5ffc60596a31e1255aaad0ac0499284bf
Devansh385994 <devansh.38594@stu.upes.ac.in>
2026-08-15 12:28:17 +0530

    Update Q4.c

--- Day_2/Q4.c
+++ Day_2/Q4.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
+//to calculate the area and circumference of the circle
+int main()
+{
+    float a;
+    printf("radius :");
+    scanf("%f",&a);
 
+    printf("circumference :%f\n",2*3.14*a);
+    printf("area :%f",3.14*a*a);
+    return 0;
+}

313eba1fbbf59c53649954f77d198a79a4d6ada7
Devansh385994 <devansh.38594@stu.upes.ac.in>
2026-08-15 21:14:41 +0530

    Implement Celsius to Fahrenheit conversion
    Fix temperature conversion formula and add input prompt.

--- Day_3/Q5.c
+++ Day_3/Q5.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+//to convert temp from celsius to farenhiet 
+int main()
+{
+    float a,b;
+    printf("enter temp :");
+    scanf("%f",&a);
+    printf("temp in farenhiet = %.2f", b= a*9/5+32);
+    return 0;
+}

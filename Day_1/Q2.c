66b73c37dde9097215d68c46ba5be1b7a7535373
Devansh385994 <devansh.38594@stu.upes.ac.in>
2026-08-15 20:04:10 +0530

    Fix arithmetic operations and error handling in Q2.c
    Corrected syntax errors and improved formatting.

--- Day_1/Q2.c
+++ Day_1/Q2.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+//showing addition,subtraction,multiplication,divison of two numbers
+int main()
+{
+float a,b;
+printf("A :");
+scanf("%f",&a);
 
+printf("B :");
+scanf("%f",&b);
+
+printf("sum :%f\n", a+b);
+printf("differnce :%f\n", a-b);
+printf("product :%f\n",a*b);
+if(b==0)
+{printf("quotient :error");}
+else
+{printf("quotient :%f\n", a/b);}
+
+return 0;
+}

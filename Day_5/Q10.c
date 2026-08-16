a07384b57bb66164d528536bee346468ea7eb5ee
Devansh385994 <devansh.38594@stu.upes.ac.in>
2026-08-16 12:08:04 +0530

    Add time conversion from seconds to h:m:s format

--- Day_5/Q10.c
+++ Day_5/Q10.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+// to convert seconds to hours:minutes:seconds
+int main()
+{
+    int h,m,s,n;
+    printf("enter time in seconds :");
+    scanf("%d",&n);
+    if(n>=0)
+   { s=n%60;
+    m=n/60;
+    h=m/60;
+    printf("time : %d hours: %d minutes: %d seconds",h,m,s);}
+    else{printf("error (time cannot be negetive)");}
+    return 0;
 
+}

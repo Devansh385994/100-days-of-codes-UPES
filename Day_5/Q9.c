122de18153512f510d2e305047f05265a28660f4
Devansh385994 <devansh.38594@stu.upes.ac.in>
2026-08-16 10:32:34 +0530

    Update Q9.c

--- Day_5/Q9.c
+++ Day_5/Q9.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+//to calculate the simple interest and compound interest
+int main()
+{
+    float p,r,t;
+    printf("enter the principle amount :");
+    scanf("%f",&p);
+    printf("enter rate of interest :");
+    scanf("%f",&r);
+    printf("enter time in years :");
+    scanf("%f",&t);
 
+    float si=(p*r*t)/100;
+    printf("simple interest :%.2f\n",si);
+    float ci=p*(pow((1+r/100),t))-p;
+    printf("compound interest :%.2f",ci);
+    return 0;
+}

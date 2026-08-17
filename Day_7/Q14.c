f1180e02aa7339aff66fe5278302da8d03bf4bcc
Devansh385994 <devansh.38594@stu.upes.ac.in>
2026-08-17 10:02:15 +0530

    Add program to check for vowels and consonants

--- Day_7/Q14.c
+++ Day_7/Q14.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+//to check whether input is vowel or consonant
+int main()
+{
+    char a;
+    printf("enter an alphabet :");
+    scanf("%c",&a);
+    if((a>='A' && a<='Z')||(a>='a' && a<='z'))
+    {
+        if((a=='a')||(a=='e')||(a=='i')||(a=='o')||(a=='u')||(a=='A')||(a=='E')||(a=='I')||(a=='O')||(a=='U'))
+        {printf("vowel\n");}
+        else{printf("consonant\n");}
+    }
+    else{printf("not an alphabet\n");}
+    return 0;
+}

bac09c25ab265dc601f4bcf8bd6f518511a622fb
Devansh385994 <devansh.38594@stu.upes.ac.in>
2026-08-18 10:16:53 +0530

    Add character type checker in Q15.c

--- Day_8/Q15.c
+++ Day_8/Q15.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+//to check whether a character is uppercase alphabet, lowercase alphabet, digit, special character
+int main()
+{
+    char ch;
+    printf("enter a character :");
+    scanf("%c", &ch);
+    if(ch>='A' && ch<='Z')
+    {printf("uppercase alphabet");}
+    else if(ch>='a' && ch<='z')
+    {printf("lowercase alphabet");}
+    else if(ch>='0' && ch<='9')
+    {printf("digit");}
+    else{printf("special character");}
+    return 0;
+}

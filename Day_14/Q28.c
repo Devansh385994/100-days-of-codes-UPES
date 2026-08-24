4aaeebea8163bc13381d414b16a3df45d87fda7b
Devansh385994 <devansh.38594@stu.upes.ac.in>
2026-08-24 11:11:06 +0530

    Add program to calculate product of even numbers
    Implement a program to calculate the product of the first n even natural numbers.

---
+++ Day_14/Q28.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// to calculate product of first n even natural numbers using loop
+int main()
+{
+    int n;
+    printf("enter the number :");
+    scanf("%d", &n);
+    int s=1;
+
+    for(int i=1; i<=n; i=i+1){
+    s = s *(2*i);}
+    printf("sum of first %d odd number is %d\n",n, s);
+    
+    return 0;
+}

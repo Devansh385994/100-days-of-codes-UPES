0539943900545a3ae6dccbf90e4e752764bb1cbb
Devansh385994 <devansh.38594@stu.upes.ac.in>
2026-09-08 18:29:07 +0530

    Create Q52.c

---
+++ Day_26/Q52.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+// to print a pattern
+int main()
+{
+    for(int i=1;i<=3;i++){
+        for(int j=1;j<=2*i-1;j++){
+            printf("*\n");
+        }printf("\n");
+    } for(int i=2;i>0;i--){
+        for(int j=1;j<=2*i-1;j++){
+            printf("*\n");
+        }printf("\n");
+    }
+    return 0;
+}

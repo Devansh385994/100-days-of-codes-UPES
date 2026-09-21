d46c3c2a1343b10b4d96ed67ef4d6d9260ee9adb
Devansh385994 <devansh.38594@stu.upes.ac.in>
2026-09-22 01:38:52 +0530

    Add program to read and print a 2D array

---
+++ Day_36/Q71.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// to read and print 2D array
+int main()
+{
+    int a[2][2] = {{1,2},{3,4}};
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+    printf("%d\t",a[i][j]);}
+    printf("\n");}
+    return 0;
+}
